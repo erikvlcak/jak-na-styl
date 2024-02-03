1f0e5319947a25e61c2945d94f62acddf6146bab
erikvlcak <erikvlcak1@gmail.com>
2024-02-03 11:51:42 +0100

    Add new components and update layout

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,20 @@
 import Hero from "./hero";
 import Card from "./card";
+import Mosaic from "./mosaic";
+import Contact from "./contact";
+import About from "./about";
+import Header from "./header";
 
 export default function App() {
     return (
         <>
             <Hero/>
+            <Header/>
+            <Mosaic/>
             <Card/>
             <Card/>
+            <About/>
+            <Contact/>
         </>
     );
 }
